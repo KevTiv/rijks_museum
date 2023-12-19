137ac4577c828c52fd7701c01c2097c2d00a7228
Kevin Tivert <tivertc.kevin@outlook.com>
2023-12-19 13:57:37 +0100

    add comment

--- src/api/rijksMuseum.ts
+++ src/api/rijksMuseum.ts
@@ -2,6 +2,17 @@ import axios from 'axios';
 import {RIJKS_KEY, RIKS_API_URL} from '@env';
 import {RijksDataApiResponse} from './types';
 
+/**
+ * Fetches art collection data from the Rijksmuseum API.
+ * @param artPieceId - The ID of the art piece to fetch.
+ * @param page - The page number to fetch.
+ * @param resultsPerPage - The number of results per page.
+ * @param imgOnly - Whether to only fetch images or not.
+ * @param artist - The name of the artist to filter by.
+ * @returns The art collection data.
+ * @throws An error if the API key or URL is not set.
+ * @throws An error if there is an API error.
+ */
 export async function getRijksArtCollection({
   artPieceId = '',
   page = 1,
